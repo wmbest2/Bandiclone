9c9032c8971d6da41257e627512ffd2f23cdbd86
Bill Best <wmbest2@gmail.com>
2010-04-26 22:06:12 -0400

    Abstract Abstract Abstract

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from direct.showbase.ShowBase import ShowBase
+import Game
  
-class MyApp(ShowBase):
+class GameLoop():
  
 	def __init__(self):
-		ShowBase.__init__(self)
+		game = Game.Game()
+		game.run()
 
-		self.environ = self.loader.loadModel("./models/crash/crash_crude.egg")
-		self.environ.reparentTo(self.render)
-		self.environ.setScale(0.25, 0.25, 0.25)
-		self.environ.setPos(0, 5, 0)
- 
-app = MyApp()
+app = GameLoop()
 app.run()
